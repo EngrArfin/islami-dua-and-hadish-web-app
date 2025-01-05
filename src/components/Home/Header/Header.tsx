849cbcaa2bf661a5cccdd1f991db2d6cefce52e1
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-06 04:15:34 +0600

    up

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-blue-600 min-h-screen flex items-center justify-center text-white px-6">
+    <header className="bg-white min-h-screen flex items-center justify-center text-black px-6">
       <div className="text-center sm:text-left max-w-4xl w-full">
         <h1 className="text-6xl sm:text-7xl font-bold leading-tight mb-6">
-          Islami Dua And Hadish Web App
+          Islami Dua And Hadith Web App
         </h1>
-        <p className="text-lg sm:text-xl text-gray-200 mb-8">
+        <p className="text-lg sm:text-xl text-gray-700 mb-8">
           Explore a rich collection of Islamic duas and hadiths, designed to
           enhance your spiritual growth and enrich your daily prayers.
         </p>
         <div className="mt-6">
           <a
             href="#explore"
-            className="bg-blue-800 hover:bg-blue-700 text-white px-10 py-4 rounded-lg text-lg sm:text-xl transition duration-300"
+            className="bg-green-500 hover:bg-green-400 text-white px-10 py-4 rounded-lg text-lg sm:text-xl transition duration-300"
           >
             Start Now
           </a>
