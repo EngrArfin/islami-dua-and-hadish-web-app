0c6a0dc91ae64e6a05c7879f9a7f6f90e01d537b
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-08 23:56:22 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Home/Header/Header";
 import NavBar from "@/components/Home/Shared/NavBar/NavBar";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      <Header />
     </div>
   );
 }
